c8edf504a4066ffc8835959a4169c01cd37cbdf1
niuniuanran <irene.anran.niu@gmail.com>
2020-07-01 21:53:44 +1200

    Implement POST router to handle uploaded file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+const multer = require("multer");
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,13 +13,19 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res)=>{
+    const file = req.file;
+    if(!file) res.send("Please upload a valid file");
+    else res.send({"name":file.originalname, "type":file.type, "size":file.size})
+})
 
 app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+    res.json({greetings: "Hello, API"});
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+    console.log('Node.js listening ...');
 });
